1cc53bea2c9ce910b4c22e0550870430a988c087
Sarmad Chandio <sarmadchandio@gmail.com>
2020-04-26 01:00:04 +0500

    ResgisterScreen without fetch API

--- Screens/Login.js
+++ Screens/Login.js
@@ -11,15 +11,13 @@ function Login({ navigation }){
     const [confirm, setConfirm] = useState(null);   //onfirms the phone  for phone Number
     const [code, setCode] = useState('123456'); // confirmation code (phone) from the user
     const [userName, setUserName] = useState('tester')
-    const [password, setPassword] = useState('givenpassword')
-    const [user, setUser] = useState(null);  //   Gets the current signed in user
+    const [password, setPassword] = useState('godmodefortestingservershouldbeon')
 
 
     // Method to fetch our own apis
 async function LoginMongoDB(method, username=null, password=null, phone=null){
     try{
-        console.log('Db')
-
+        
         let packet = {
             'method' : method,
         }
@@ -30,7 +28,7 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
             packet.username = username
             packet.password = password
         }
-
+        console.log(packet)
         const response = await fetch('https://medchatse.herokuapp.com/login', {
             method: 'POST',
             headers:{
@@ -38,12 +36,14 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
             },
             body: JSON.stringify(packet)
         })
-        const json = await response.json()
-        console.log("JSON Returned: ", json)
-        console.log("Type of json: ", typeof(json))
-        setUser(json)
-        navigation.navigate('Home', response)
-    } catch(error){
+        const profileInfo = await response.json()
+        console.log("JSON Returned: ", profileInfo)
+        if(!profileInfo.message)
+            navigation.navigate('Home', profileInfo)
+        else
+            alert(profileInfo)
+    }catch(error){
+        alert(error)
         console.log(error)
     }
 }
@@ -53,7 +53,6 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
         try{
             const verification = await auth().signInWithPhoneNumber(phoneNumber);
             setConfirm(verification);
-            setUser({'phoneNumber': phoneNumber})
         }catch(err){
             alert(err)
             console.log('signInPhoneERR: ', err)
@@ -66,7 +65,11 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
             console.log('confirmed', conf)
             //Phone Number Verified so a request can be sent to the server to fetch data
             if (conf){
-                LoginMongoDB('phone','user','pass', phoneNumber)
+                await LoginMongoDB('phone','user','pass', phoneNumber)
+
+                // The user is signed in after verfying the confirm code.
+                // So we need to sign out the user from fire_base.
+                auth().signOut().then(console.log('sigend_out...?'))
             }
             else{
                 alert('Invalid Verification Code')
@@ -78,25 +81,8 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
         }
     }
 
-    // This function keeps updating user values (whenever auth() is called)
-    function onAuthStateChanged(user) {
-        console.log('settingUser: ', user)
-        setUser(user);
-      }
-
-    // UseEffect is run after each render...?
-    //The same componentWillUnmount job can be achieved by optionally returning
-    // a function from our useEffect() parameter:
-    useEffect(()=>{
-        console.log('UseEffect ', user)
-        // setPhoneNumber('+16505551234')
-        // console.log("UserEffect-->user.displayName:", user.displayName)
-        // const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-        // return subscriber; // unsubscribe on unmount
-    }, [user])
-
     // Assuming the user is signed out, if there is no display name...?
-    if (!user){
+    if (!confirm){
         return(
             <View>
                 <Text>Login Screen</Text>
@@ -109,7 +95,7 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
                     placeholder='Password' 
                     secureTextEntry={true}
                     onChangeText={(text) => setPassword(text)}
-                    value={password}    
+                    value={password}
                     />
                 <TextInput 
                     placeholder='phone("+16505551234")'
@@ -131,8 +117,8 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
             </View>
         );
     }
-    // Only phone is authentic 
     
+    // Only phone is authentic 
     return(
         // If the user is already logged IN? re route to HomeScreen
         // CallHome Screen here and do all this in HomeScreen
@@ -140,18 +126,10 @@ async function LoginMongoDB(method, username=null, password=null, phone=null){
             <Text>Enter the code below!</Text>
             <TextInput placeholder = 'Enter the code' value={code} onChangeText={text=>setCode(text)}/>
             <Button title = 'Submit code' onPress={()=>confirmCode() }/>
-            <Button title='Back' onPress={()=>{setUser(null)}} />
+            <Button title='Back' onPress={()=>{setConfirm(null)}} />
         </View>
     )
-    
 }
-
-function signOut(){
-    auth()
-        .signOut()
-        .then(() => console.log('User signed out!: '))
-}
-
 
 
 export default Login
--- Screens/Register.js
+++ Screens/Register.js
@@ -1,13 +1,167 @@
-import React from 'react'
-import {View, Text, Button} from 'react-native'
+import React, {useState, }from 'react'
+import {View, Text, Button, StyleSheet} from 'react-native'
+import { TextInput } from 'react-native-gesture-handler';
 
 
 function Register({ navigation }){
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName]= useState('')
+    const [userName, setUserName]= useState('')
+    const [password, setPassword] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [email, setEmail] = useState('')
+    const [pic, setPic] = useState('')
+    const [diseaseHist, setDiseaseHist]= useState('')
+    const [bday, setBday]= useState('')
+    const [validFName, setValidFName] = useState(true)
+    const [validLName, setValidLName] = useState(true)
+    const [validUName, setValidUName] = useState(true)
+    const [validPass, setValidPass] = useState(true)
+    const [validPhone, setValidPhone] = useState(true)
+    const [validEmail, setValidEmail] = useState(true)
+    const [validBday, setValidBday] = useState(true)
+    const [validAll, setValidAll] = useState(false)
+ 
+
+
+    
+    // validates all inputs before sending to Server
+    function ValidateAndSend(){
+        validateInput(firstName, 'fName')
+        validateInput(lastName, 'lName')
+        validateInput(userName, 'userName')
+        validateInput(password, 'password')
+        validateInput(phoneNumber, 'phoneNumber')
+        validateInput(email, 'email')
+        validateInput(bday, 'bday')
+        if( validEmail && 
+            validFName &&
+            validLName &&
+            validUName &&
+            validPass &&
+            validPhone &&
+            validBday ) setValidAll(true)
+        else setValidAll(false)
+
+        if(validAll){
+
+        }
+    }
+
+    // Checks the input at client side before sending it to the server
+    function validateInput(text, type){
+        // starts with a letter, can contain alphanumeric afterwards
+        if(type == 'userName'){
+            setUserName(text)
+            const check = /^[a-zA-Z]\w*$/
+            setValidUName(check.test(text))
+        }
+        // contains only letters
+        else if(type == 'fName'){
+            setFirstName(text)
+            const check = /[a-zA-Z]+$/
+            setValidFName(check.test(text))
+        }
+        // contains only letters
+        else if(type == 'lName'){
+            setLastName(text)
+            const check = /[a-zA-Z]+$/
+            setValidLName(check.test(text))
+        }
+        // must be atleast 5 chars long
+        else if(type=='password'){
+            setPassword(text)
+            const check = /.{5,}/
+            setValidPass(check.test(text))
+        }
+        // covers valid email styles
+        else if(type=='email'){
+            setEmail(text)
+            // Taken from internet
+            const check = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+            setValidEmail(check.test(text))
+        }
+        // for now +92[PakistaniNumbers]
+        else if(type=='phoneNumber'){
+            setPhoneNumber(text)
+            const check = /\+92[0-9]{10}/
+            setValidPhone(check.test(text))
+        }
+        else if(type=='bday'){
+            setBday(text)
+            const check = /[0-9]{4}-[0-9]{2}-[0-9]/
+            setValidBday(check.test(text))
+        }
+    }
+
     return(
-        <View>
+        <View >
             <Text>Welcome to RegisterScreen</Text>
+            <TextInput 
+                placeholder='First Name (start with a letter, can contain alphanumeric afterwards)' 
+                value={firstName} 
+                onChangeText={(text)=> validateInput(text, 'fName')}
+                style={validFName?styles.inputBox:styles.error}
+                />
+            <TextInput 
+                placeholder='Last Name (letters only)' 
+                value={lastName}
+                onChangeText={(text)=> validateInput(text, 'lName')}
+                style={validLName?styles.inputBox:styles.error}
+                />
+            <TextInput 
+                placeholder='User Name (letters only)' 
+                style={validUName?styles.inputBox:styles.error}
+                value={userName}
+                onChangeText={(text)=> validateInput(text, 'userName')}
+                />
+            <TextInput 
+                placeholder='Password (Atleast 5 digits long)' 
+                secureTextEntry={true} 
+                value={password}
+                onChangeText={(text)=> validateInput(text, 'password')}
+                style={validPass?styles.inputBox:styles.error}
+                />
+            <TextInput 
+                placeholder='Phone Number (+92[10 more digits])' 
+                value={phoneNumber}
+                onChangeText={(text)=> validateInput(text, 'phoneNumber')}
+                style={validPhone?styles.inputBox:styles.error}
+                />
+            <TextInput 
+                placeholder='Email'
+                value={email}
+                onChangeText={(text)=> validateInput(text, 'email')}
+                style={validEmail?styles.inputBox:styles.error}
+                />
+            <TextInput 
+                placeholder='Birth Day (yyyy-mm-dd)'
+                value={bday}
+                onChangeText={(text)=> validateInput(text, 'bday')}
+                style={validBday?styles.inputBox:styles.error}
+                />
+            
+            <Button 
+                title='Register'
+                onPress={()=> ValidateAndSend()}
+            />
+            {validAll ? null : <Text>Atleast One of the blocks above ahs an error</Text>}
         </View>
     )
+
 }
+
+const styles = StyleSheet.create({
+    inputBox: {
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 1,
+    },
+    error: {
+        borderWidth: 1,
+        borderColor: 'red',
+        marginBottom: 1,
+    }
+})
 
 export default Register;
